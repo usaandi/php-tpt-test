2622ead1da81a8217cb605a2d20f074c38f61f2b
Siim Tiilen <eritikass@gmail.com>
2018-05-11 09:07:30 +0300

    fix

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,4 @@
 <?php
-declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
@@ -8,4 +7,4 @@ final class ExampleTest extends TestCase
     public function testSomething(): void {
         $this->assertEquals("a", "a");
     }
-)
+}
